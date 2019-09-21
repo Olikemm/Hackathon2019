959a7a28cb34325ce8e5a6450fd2ca7aae837dc5
Leoliz <leo.scheubel@gmail.com>
2019-09-21 13:57:24 +0200

    LS Workspace Update Look

--- Assets/_Workspace_LS/Prefabs/Spieler/Scripts/Player.cs
+++ Assets/_Workspace_LS/Prefabs/Spieler/Scripts/Player.cs
@@ -4,31 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    /*var anteil = 0.05f; //5% an jeder Kante
-    var xSpace = Screen.width * anteil;
-    var ySpace = Screen.height * anteil;*/
     public float walkSpeed = 6f;
     public float speedSmoothing = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationZ;
-        transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX;
+        
     }
        // Update is called once per frame
     void Update()
     {
+        float movement = Input.GetAxis ("Vertical");
+		movement *= Time.deltaTime;
+
+		this.transform.Translate 
+		(Vector3.forward * walkSpeed * movement);
+         
         transform.rotation = Quaternion.identity;
-        if(Input.GetKey(KeyCode.W))
-            transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
-        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
-            transform.Translate(Vector3.forward * speedSmoothing * Time.deltaTime);
-        if(Input.GetKey(KeyCode.A))
-            transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
-        if(Input.GetKey(KeyCode.S))
-            transform.Translate(Vector3.back * walkSpeed * Time.deltaTime);
-        if(Input.GetKey(KeyCode.D))
-            transform.Translate(Vector3.right * walkSpeed * Time.deltaTime);
+        
         if(Input.GetKey(KeyCode.Space))
             transform.Translate(Vector3.up * walkSpeed * Time.deltaTime);
         
