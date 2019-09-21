dfaa57b4f062159e95bde6699128a38f48a40d23
Leoliz <leo.scheubel@gmail.com>
2019-09-21 22:47:37 +0200

    Inventory Update

---
+++ Assets/_Workspace_LS/HUD/Scripts/InventarZeahlerHolz.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventarZeahlerHolz : MonoBehaviour
+{
+    Text ResourcenStand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResourcenStand = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ResourcenStand.text = "Holz: " + Inventory.instance.getHolz();
+    }
+}
---
+++ Assets/_Workspace_LS/Prefabs/Spieler/Scripts/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory instance;
+    [SerializeField] public float Holz;
+    [SerializeField] public float Stein;
+    void Awake()
+    {
+        instance = this;
+    }
+    public void setHolzPlus1()
+    {
+        Holz++;
+    }
+    public void setSteinPlus1()
+    {
+        Stein++;
+    }
+    public void setHolzMinus1()
+    {
+        Holz--;
+    }
+    public void setSteinMinus1()
+    {
+        Stein--;
+    }
+    public float getHolz()
+    {
+        return Holz;
+    }
+    public float getStein()
+    {
+        return Stein;
+    }
+}
